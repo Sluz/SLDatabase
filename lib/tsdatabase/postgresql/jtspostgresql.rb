e796fb63aea5ad9a39e3f2bd9e41ff8f35acce50
Cyril <cyril@tapastreet.com>
2014-07-18 13:25:36 +0100

    use default initialize

--- lib/tsdatabase/postgresql/jtspostgresql.rb
+++ lib/tsdatabase/postgresql/jtspostgresql.rb
@@ -9,23 +9,5 @@ require 'tsdatabase/tsposgresql'
 # \author Cyril Bourgès <cyril@tapastreet.com>
 #
 module TSDatabase
-  class JTSPostgresql < TSPostgresql
-    def initialize option={}
-      @dbconfig = {
-        :username => option["username"],
-        :password => option["password"],
-        :database => option["database"]
-      }
-      
-       if (option["url"].nil?)
-        if (option["port"].nil?)
-          @dbconfig[:url] = "jdbc:postgres://#{ option["username"] }:#{ option["password"] }@#{ option["host"] }/#{ option["database"] }"
-        else
-          @dbconfig[:url] = "jdbc:postgres://#{ option["username"] }:#{ option["password"] }@#{ option["host"] }:#{ option["port"] }/#{ option["database"] }"
-        end
-      else 
-        @dbconfig[:url] = "jdbc:"+option["url"]
-      end
-    end
-  end
+  class JTSPostgresql < TSPostgresql; end
 end
