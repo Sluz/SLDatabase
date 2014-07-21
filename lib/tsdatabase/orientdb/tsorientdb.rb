f29fc2a239e8af3a42921ba1d5f3416019512379
Cyril <cyril@tapastreet.com>
2014-07-21 18:19:18 +0100

    fix quote

--- lib/tsdatabase/orientdb/tsorientdb.rb
+++ lib/tsdatabase/orientdb/tsorientdb.rb
@@ -1,5 +1,5 @@
-#  Tapastreet ltd 
-#  All right reserved 
+#  Tapastreet ltd
+#  All right reserved
 #
 
 require 'orientdb4r'
@@ -10,42 +10,42 @@ require 'tsdatabase/tsclientdb'
 #
 module TSDatabase
   class TSOrientdb < TSClientdb
-     
+
     def initialize option={}
       @dbconfig = {
         :user => option["username"],
         :password => option["password"],
         :database => option["database"],
       }
-      
+
       @server_config = {
-        :host => option["host"], 
+        :host => option["host"],
         :port => option["port"],
         :ssl  => option["ssl"]
       }
-      
+
       #if (option["url"].nil?)
       #  if (option["port"].nil?)
       #    @dbconfig[:url] = "remote:#{ option["host"] }/#{ option["database"] }"
       #  else
       #    @dbconfig[:url] = "remote:#{ option["host"] }:#{ option["port"] }/#{ option["database"] }"
       #  end
-      #else 
+      #else
       #  @dbconfig[:url] = option["url"]
       #end
-      
+
       @db = Orientdb4r.client @server_config
     end
-    
+
     def id
-      
+
     end
-    
+
     # \return true if query is a record_id
     def is_by_id? query
       query.is_a?(String) && (query =~ /\A#\d+:\d+\z/)
     end
-    
+
     # \return hash of records or nil
     def find_by_id record_id, *option
       begin
@@ -64,7 +64,7 @@ module TSDatabase
         nil
       end
     end
-    
+
     # \return a array whith hash of record
     def find_by_hash record_datas, *option
       connect
@@ -74,20 +74,20 @@ module TSDatabase
         if key === "@class"
           from = value
         else
-          where += key.to_s+"=#{ @db.quote(value) }"
+          where += key.to_s+"=#{ quote(value) }"
         end
       end
-      
+
       if from.empty?
         unless option.empty?
           from = option[0]
         end
       end
-      
+
       @db.query("select from #{from} where #{where}")
-      
+
     end
-    
+
     # \return a array whith hash of record
     def find_by_query query, *option
       connect
@@ -97,7 +97,7 @@ module TSDatabase
         @db.query(query, option[0])
       end
     end
-    
+
     #\return boolean exception is false or exception if failed and exception is true
     def create hash, exception = true
       begin
@@ -112,7 +112,7 @@ module TSDatabase
       end
       true
     end
-    
+
     #\return boolean exception is false or exception if failed and exception is true
     def update hash, exception = true
       begin
@@ -128,11 +128,11 @@ module TSDatabase
       end
       true
     end
-    
+
     #\return boolean exception is false or exception if failed and exception is true
     def remove record_id, exception = true
       unless is_by_id?(record_id); if exception; raise RecordIdError  else false end end
-      
+
       begin
         connect
         @db.delete_document(record_id)
@@ -145,7 +145,7 @@ module TSDatabase
       end
       true
     end
-    
+
     def connect
       unless connected?
         begin
@@ -155,7 +155,7 @@ module TSDatabase
         end
       end
     end
-    
+
     #\return true if connection is alive
     def connected?
       @db.connected?
@@ -166,34 +166,62 @@ module TSDatabase
         @db.disconnect
       end
     end
-    
+
+    def quote value
+      case value
+      when Numeric, Symbol
+        value.to_s
+      when String
+        quote_string(value)
+      when Array
+        "[" + value.map { |x| quote(x) }.join(", ") + "]"
+      when Regexp
+        quote_regexp(value)
+      else
+        quote_string value.to_s
+      end
+    end
+
+    def quote_string string
+      if(string[0, 1] == "'" && string[-1, 1] == "'")
+        string
+      else
+        string.gsub!("'", "\'")
+        "'#{ string }'"
+      end
+    end
+
+    def quote_regexp regexp
+      "'#{ regexp.source }'"
+    end
+
     def parse_id_from hash
       hash["@rid"]
     end
-    
+
     def parse_exception exception
       except = nil
-      
+
       #Duplicate record (Java)
       if exception.message["com.orientechnologies.orient.core.storage.ORecordDuplicatedException"].nil? == false
         except = RecordDuplicateError.new  exception.message
         except.set_backtrace(exception.backtrace)
-        
-      #IO Database Connection (Java)
+
+        #IO Database Connection (Java)
       elsif exception.message["com.orientechnologies.common.io.OIOException"].nil? == false
         except = ConnectionError.new exception.message
         except.set_backtrace(exception.backtrace)
-        
-      #IO Database Connection (Ruby)
+
+        #IO Database Connection (Ruby)
       elsif exception.message["all nodes failed to communicate with server!"].nil? == false
         except = ConnectionError.new exception.message
         except.set_backtrace(exception.backtrace)
-        
-      #default
+
+        #default
       else
         except = super exception
       end
-      
+
       except
     end
   end
